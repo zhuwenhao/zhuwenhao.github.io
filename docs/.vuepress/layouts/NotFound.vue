48c896e65b25ebd780c275743887adb15ff228d4
zhuwenhao <m@zhuwenhao.me>
2023-11-01 12:26:19 +0800

    ...

---
+++ docs/.vuepress/layouts/NotFound.vue
@@ -0,0 +1,10 @@
+<!-- .vuepress/components/BlogHero.vue -->
+<script setup lang="ts">
+import NotFound from "vuepress-theme-hope/layouts/NotFound.js";
+</script>
+
+<template>
+  <NotFound>
+    <img src="/images/404.png" style="width: min(100%, 500px);" />
+  </NotFound>
+</template>
